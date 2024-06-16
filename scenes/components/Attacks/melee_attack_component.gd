ce2a86cb22a649294d0ffda32bcdaa7238d4804c
Ramon Barros <pbarrosramon@gmail.com>
2024-06-16 03:51:41 -0300

    dio-4 adjust drop parameters and add 20 minutes time-lapse

--- scenes/components/Attacks/melee_attack_component.gd
+++ scenes/components/Attacks/melee_attack_component.gd
@@ -18,7 +18,7 @@ func is_range_attack_player() -> bool:
 	return false
 	
 func attack(target_position: Vector2) -> void:
-	var direction_to_player = (target_position - position).normalized()
+	var direction_to_player = (target_position - enemy.position).normalized()
 	if is_range_attack_player():
 		target.get_hit(enemy.hit_damage, direction_to_player)
 	target = null
--- scripts/ui/game_ui.gd
+++ scripts/ui/game_ui.gd
@@ -1,7 +1,18 @@
 extends Node
 
+var end_time = 20
+
 func _ready():
-	$Panel/Label.text = "Points: %09d" % [GameManager.points]
+	$PanelPoints/Points.text = "Points: %09d" % [GameManager.points]
+	$PanelTime/Time.text = "Time: %02d:%02d" % [end_time, 0]
 	
 func _process(delta):
-	$Panel/Label.text = "Points: %09d" % [GameManager.points]
+	$PanelPoints/Points.text = "Points: %09d" % [GameManager.points]
+	var time_left = (end_time * 60) - GameManager.time_elapsed
+	if time_left <= 0:
+		GameManager.is_game_over = true
+		return
+	var sec = fmod(time_left, 60.0)
+	var min = int(time_left / 60)
+	$PanelTime/Time.text = "Time: %02d:%02d" % [min, sec]
+	
