74240ed8631ca53d6d452e0f8c3d6c797ecace0f
Ramon Barros <pbarrosramon@gmail.com>
2024-07-02 18:15:07 -0300

    DIO-13 implements authentication to firebase and new ranking screen

--- scripts/login_script/login_example.gd
+++ scripts/login_script/login_example.gd
@@ -7,5 +7,12 @@ func _ready():
 		"email", "password"
 	)
 	
-func on_login_failed():
-	get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
+func on_login_failed(code, message):
+	print("Login failed!!!")
+	print("Code: " + str(code))
+	print("Message:" + message)
+	print("Make sure you have configured the login credentials at login.gd")
+	get_tree().quit()
+
+func on_login_succeed():
+	print("Login succeeded!!!")
