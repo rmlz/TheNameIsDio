d58ae2ecc650c0e8ea53a2b7c23662b01b1af4fe
Ramon Barros <pbarrosramon@gmail.com>
2024-09-16 10:11:12 -0300

    DIO-24 implements item lunge_boost

--- scene_ui/shop/resources/scripts/item_resource/shop_item_resource.gd
+++ scene_ui/shop/resources/scripts/item_resource/shop_item_resource.gd
@@ -3,11 +3,18 @@ class_name ShopItemResource
 
 func apply(character: CharacterBase) -> void:
 	match self.id:
-		"arcane_amplifier": _add_arcane_aplifier(character)
-		_: print("Apply item not implemented. ID: " + self.id)
+		"arcane_amplifier": _add_arcane_amplifier(character)
+		"lunge_boost": _add_lunge_boost(character)
+		_: print("Apply item method not implemented for ID: " + self.id)
 			
-func _add_arcane_aplifier(character: CharacterBase):
+func _add_arcane_amplifier(character: CharacterBase):
 	character.inventory_component.add_item(self)
 	character.melee_attack_component.attack_area.scale = Vector2(2, 1)
 	if (GameManager.is_debug_enabled):
 		print("Item applied: " + self.id)
+		
+func _add_lunge_boost(character: CharacterBase):
+	assert(character is PlayerObject)
+	character.inventory_component.add_item(self)
+	if GameManager.is_touch_joypad_enabled:
+		(character as PlayerObject).mobile_joypad.show_touch_dash_button()
